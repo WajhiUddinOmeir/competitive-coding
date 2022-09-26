187719544ba1e83cf253b699a2f9e7c6bbf07363
WajhiUddinOmeir <108371272+WajhiUddinOmeir@users.noreply.github.com>
2022-09-26 15:34:18 +0530

    Time: 35 ms (13.69%), Space: 16 MB (18.13%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,52 +1,37 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> v;
-int n = nums.size();
-int x = n/3;
-if(n<2){
-return nums;
-}
-if(n==2){
-if(nums[0]==nums[1]){
-v.push_back(nums[0]);
-return v;
-}
-return nums;
-}
-//Optimal Approach Boyer Moore Voting Algorithm
-int num1=INT_MIN, num2=INT_MIN, c1=0,c2=0;
-for(int i =0;i<n;i++){
-if(nums[i] == num1){
-c1++;
-} else if (nums[i] == num2){
-c2++;
-} else if(c1==0){
-num1 = nums[i];
-c1=1;
-} else if(c2==0){
-num2 = nums[i];
-c2=1;
-} else{
-c1--;
-c2--;
-}
-}
-c1=0;
-c2=0;
-for(int i =0;i<n;i++){
-if(nums[i] == num1){
-c1++;
-} else if(nums[i] == num2){
-c2++;
-}
-}
-if(c1>x){
-v.push_back(num1);
-}
-if(c2>x){
-v.push_back(num2);
-}
-return v;
+        int n=nums.size();
+         int sz = n/3;
+  int num1 = -1, num2 = -1, count1 = 0, count2 = 0, i;
+  for (i = 0; i < n; i++) {
+    if (nums[i] == num1)
+      count1++;
+    else if (nums[i] == num2)
+      count2++;
+    else if (count1 == 0) {
+      num1 = nums[i];
+      count1 = 1;
+    } else if (count2 == 0) {
+      num2 = nums[i];
+      count2 = 1;
+    } else {
+      count1--;
+      count2--;
+    }
+  }
+  vector < int > ans;
+  count1 = count2 = 0;
+  for (i = 0; i < n; i++) {
+    if (nums[i] == num1)
+      count1++;
+    else if (nums[i] == num2)
+      count2++;
+  }
+  if (count1 > sz)
+    ans.push_back(num1);
+  if (count2 > sz)
+    ans.push_back(num2);
+  return ans;
     }
 };
